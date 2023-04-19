ecc69838d5355e9404171f6a1c19d865bdefa028
AlapeAniruddha <62590590+AlapeAniruddha@users.noreply.github.com>
2023-04-19 13:12:28 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -11,13 +11,15 @@ num_3 = st.number_input("Enter the third number",step=1)
 
 def largest_num(num_1,num_2,num_3):
     if num_1 > num_2 and num_1 > num_3:
-        st.write('num_1 = ',num_1)
+        st.write('The largest number is the first number = ',num_1)
     elif num_2 > num_3 and num_2 > num_1:
-        st.write('num_2 = ',num_2)
+        st.write('The largest number is the second number = ',num_2)
     elif num_3 > num_2 and num_3 > num_1:
-        st.write('num_3 = ',num_3)    
-    else: 
-        st.write('Equal numbers')  
+        st.write('The largest number is the third number = ',num_3)    
+    elif num_3 == num_2 and num_3 == num_1:
+        st.write('All the numbers are equal and the value is = ',num_3)
+    elif num_3 == num_2 and num_3 != num_1: 
+        st.write('Two numbers are equal and the value is = ',num_3)  
 
 if st.button('Find Largest'):
     largest_num(num_1,num_2,num_3)
