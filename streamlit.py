6872f55fdea090f654eebe97127b32166771f3fa
AlapeAniruddha <62590590+AlapeAniruddha@users.noreply.github.com>
2023-04-19 12:58:00 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-num_1 = st.number_input("The first number",min_value=-30000,max_value=0,step=1)
-num_2 = st.number_input("The second number",min_value=-20000,max_value=400000 ,step=1)
-num_3 = st.number_input("The third number",min_value=0,max_value=20,step=1)
+num_1 = st.number_input("Enter the first number",step=1)
+num_2 = st.number_input("Enter the second number",step=1)
+num_3 = st.number_input("Enter the third number",step=1)
 
 if num_1 > num_2 and num_1 > num_3:
     st.write('num_1')
